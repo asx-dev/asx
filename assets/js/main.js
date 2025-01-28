92300caea2645775e218fa5ac0a2973b6343f6e7
asx-dev <asxdev18@gmail.com>
2025-01-28 18:06:12 +0000

    Dark/Light theme and Responsive design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-// Nav Menu Logic
+// Navigation
 const openMenuButton = document.getElementById("open-button");
 const closeMenuButton = document.getElementById("close-button");
-const menu = document.querySelector(".nav");
+const menu = document.querySelector(".mobile__menu");
 
 openMenuButton.addEventListener("click", () => {
   menu.classList.add("open");
@@ -9,4 +9,13 @@ openMenuButton.addEventListener("click", () => {
 
 closeMenuButton.addEventListener("click", () => {
   menu.classList.remove("open");
+});
+
+// Up arrow
+const upButton = document.querySelector(".up__arrow");
+
+window.addEventListener("scroll", () => {
+  window.scrollY > 50
+    ? upButton.classList.add("show")
+    : upButton.classList.remove("show");
 });
